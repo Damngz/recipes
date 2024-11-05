31e9f47e7af02ae5f513ed1dfff62f8bf6725f76
Damngz <da.munozgz@gmail.com>
2024-11-04 21:08:05 -0300

    fix: search all

--- src/main/java/com/recipes/recipes/security/SecurityConfig.java
+++ src/main/java/com/recipes/recipes/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
       .authorizeHttpRequests((requests) -> requests
-        .requestMatchers("/", "/recetas", "/login", "/**.css", "/**.js").permitAll()
+        .requestMatchers("/", "/recetas", "/login", "/buscar", "/**.css", "/**.js").permitAll()
         .anyRequest().authenticated()
       )
       .formLogin((form) -> form
